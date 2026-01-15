0bc4ba356d0172e12c38ada13f531c3455e1dd51
Paulo Scherma <pauloantonioschermapinto@gmail.com>
2026-01-15 12:01:30 +0000

    API Workings

--- Projeto/app/src/main/java/com/example/projeto/LoginActivity.java
+++ Projeto/app/src/main/java/com/example/projeto/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.example.projeto;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +17,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText etEmail, etPassword;
+    private static final String TOKEN = "TOKEN";
+    //private String baseUrl = "http://10.0.2.2/projeto/projeto_v1/backend/web/api";
+    private String baseUrl = "http://172.22.21.234/Projeto/projeto_v1/backend/web/api";
+
+    private final String loginEndpoint = baseUrl + "/user/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,21 +52,42 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View view) {
-        String email=etEmail.getText().toString();
-        String pass=etPassword.getText().toString();
+        String email = etEmail.getText().toString();
+        String pass = etPassword.getText().toString();
 
-        if (!isEmailValido(email)){
-            etEmail.setError("Email invalido");
-            return;
-        }
-        if (!isPasswordValida(pass)){
+
+        if (!isPasswordValida(pass)) {
             etPassword.setError("Password invalida");
             return;
         }
-        Intent intent = new Intent(this, MenuHomeActivity.class);
-        intent.putExtra("EMAIL", email);
-        startActivity(intent);
-        finish();
+
+        JSONObject loginBody = new JSONObject();
+        try {
+            loginBody.put("username", email);
+            loginBody.put("password", pass);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, loginEndpoint, loginBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Sucesso: Navegar para Home
+                        Intent intent = new Intent(LoginActivity.this, MenuHomeActivity.class);
+                        intent.putExtra("EMAIL", email);
+                        startActivity(intent);
+                        finish();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(LoginActivity.this, "Erro no Login: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        Volley.newRequestQueue(this).add(request);
     }
 
     public void onClickCreateAccount(View view) {
@@ -56,16 +96,15 @@ public class LoginActivity extends AppCompatActivity {
         finish();
     }
 
-    private boolean isEmailValido(String email){
-        if (email==null)
+    private boolean isEmailValido(String email) {
+        if (email == null)
             return false;
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
-    private boolean isPasswordValida(String pass){
-        if (pass==null)
+    private boolean isPasswordValida(String pass) {
+        if (pass == null)
             return false;
-        return pass.length()>=4;
+        return pass.length() >= 4;
     }
-
 }
